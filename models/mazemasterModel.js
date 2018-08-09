a40d34003e22a20da67e9b4c8478953bd6fd88e4
Tathmesh <tathmesh@github.com>
2018-08-08 22:30:28 -0400

    Added a grid of references and a dictionary that counts the number of each tile type that is on the screen.

--- models/mazemasterModel.js
+++ models/mazemasterModel.js
@@ -10,12 +10,40 @@
 //////////////////////// MazeMaster Model ////////////////////////
 
 let mazemasterModel = {
-    playerTiles: 0,
-    monsterTiles: 0,
-    wallTiles: 0,
     paintbrushTile: 'N',
     palette: {}
 };
+
+/// Generate a dictionary that counts the number of each tile that has been
+///     drawn on the screen.
+mazemasterModel.tileCount = {};
+
+/// Use a for loop to iterate through both lowercase and uppercase alphabet to
+///     add valid tileCode. This approach is less efficient but is more robust
+///     because you don't need to manually add new entries.
+for (let c = 0; c < 26; ++c)
+{
+    if (gridModel.isValidTileCode(String.fromCharCode(97 + c)))
+    {
+        mazemasterModel.tileCount[String.fromCharCode(97 + c)] = 0;
+    }
+    if (gridModel.isValidTileCode(String.fromCharCode(65 + c)))
+    {
+        mazemasterModel.tileCount[String.fromCharCode(65 + c)] = 0;
+    }
+}
+
+/// Generate a 2D array of references to DOM nodes, which will be used
+///     by the Erase paintbrush option to delete DOM nodes on the screen.
+mazemasterModel.canvas = new Array(gridModel.tilesPerSide);
+for (let y = 0; y < gridModel.tilesPerSide; ++y)
+{
+    mazemasterModel.canvas[y] = new Array(gridModel.tilesPerSide);
+    for (let x = 0; x < gridModel.tilesPerSide; ++x)
+    {
+        mazemasterModel.canvas[y][x] = null;
+    }
+}
 
 mazemasterModel.palette['P'] = function(coordX, coordY) {
     /// Adds a Player to gridModel.actorsGrid and attaches its DOM Node
@@ -30,7 +58,8 @@ mazemasterModel.palette['P'] = function(coordX, coordY) {
                            .css("top", (50 * coordY) + "px")
                            .css("z-index", 10)
                            .appendTo("#grid");
-        ++mazemasterModel.playerTiles;
+        mazemasterModel.canvas[coordY][coordX] = playerModel.node;
+        mazemasterModel.tileCount[mazemasterModel.paintbrushTile] += 1;
     }
 }
 
@@ -41,9 +70,20 @@ mazemasterModel.palette['W'] = function(coordX, coordY) {
     /// anon : int int -> void
     /// requires: 0 <= coordX, coordY < gridModel.tilesPerSide
 
-    gridModel.addActor('W', coordX, coordY);
-    Wall(coordX, coordY);
-    ++mazemasterModel.wallTiles;
+    if (mazemasterModel.canvas[coordY][coordX] === null)
+    {
+        gridModel.addActor('W', coordX, coordY);
+        let newWall = new Wall(coordX, coordY);
+        mazemasterModel.canvas[coordY][coordX] = newWall.node;
+        mazemasterModel.tileCount[mazemasterModel.paintbrushTile] += 1;
+    }
+}
+
+mazemasterModel.palette['N'] = function(coordX, coordY) {
+    if (mazemasterModel.canvas[y][x] !== null)
+    {
+        $(mazemasterModel.canvas[coordY][coordX]).remove();
+    }
 }
 
 mazemasterModel.changePaintbrush = function(newTile) {
