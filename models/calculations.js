37ddad79aefb59ee479a0065d907e317e4094552
Tathmesh <tathmesh@github.com>
2018-08-17 22:16:48 -0400

    Removed display functions from calculations.js

--- models/calculations.js
+++ models/calculations.js
@@ -25,30 +25,3 @@ function getDiffVector(endVector, startVector) {
     
     return diffVector;
 }
-
-function debugTile(x, y, id)
-{
-    /// Constructor for a DOM Node representing a tile to 
-    ///   visualize the coordinate system.
-
-    var tile = $(document.createElement("div"));
-    tile.attr("class", "tile")
-        .attr("id", id)
-        .css("left", (50 * x) + "px")
-        .css("top", (50 * y) + "px");
-    tile.appendTo("#grid");
-    return tile[0];
-}
-
-function makeCheckerBoard()
-{
-    for (var x = 0; x < 1000 / 50; ++x)
-    {
-        for (var y = 0; y < 1000 / 50; ++y)
-        {
-            var id = "debugTileA";
-            if ((x + y) % 2 == 0) id = "debugTileB";
-            debugTile(x, y, id);
-        }
-    }
-}
